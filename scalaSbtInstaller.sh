65caed15730fa51e80cc4e974a6071965cd56326
ldipotetjob <ldipotet.job@gmail.com>
2016-10-04 09:47:05 +0200

    changing our comment to english language

--- scalaSbtInstaller.sh
+++ scalaSbtInstaller.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-##  este es un script para la version 2.11.7 scala en cloud9 (https://c9.io)
-##  de querer otra version ir a http://www.scala-lang.org/files/archive/scala-2.11.7.deb
-##  importante que sea una version para debian ya que lo que tiene en cloud9 es ubuntu
+##  This script is for version 2.11.7 of scala en cloud9 (https://c9.io)
+##  if you need another version go to the site http://www.scala-lang.org/files/archive/scala-2.11.7.deb
+##  is important that it be a debian version, because OS in cloud9 is ubuntu
 
 sudo apt-get remove scala-library scala
 wget http://www.scala-lang.org/files/archive/scala-2.11.7.deb
@@ -10,10 +10,10 @@ sudo dpkg -i scala-2.11.7.deb
 sudo apt-get update
 sudo apt-get install scala
 
-## instalacion de sbt para la 2.11.7 de scala 
-## suponemos que no hay vesriones anteriores de sbt 
-## en caso contrario : sudo apt-get purge sbt ó sudo apt-get remove sbt 
-## en mi caso preferiblemente purge pero si hay dudas sobre lo instalado mejor la opcion de remove 
+## sbt installation for scala version 2.11.7  
+## we assume that sbt is not installed yet
+## in other case : sudo apt-get purge sbt ó sudo apt-get remove sbt 
+## I prefer purge, but if you have any doubt about software installed, we suggest remove option  
 
 wget http://dl.bintray.com/sbt/debian/sbt-0.13.9.deb
 sudo dpkg -i sbt-0.13.9.deb 
